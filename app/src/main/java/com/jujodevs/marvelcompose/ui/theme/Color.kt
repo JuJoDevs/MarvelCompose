d127069aeb32dd42f564c78aac51d25960783de3
jujob <jujo.bonillo90@gmail.com>
2023-11-24 15:38:26 +0100

    Theme Colors

--- app/src/main/java/com/jujodevs/marvelcompose/ui/theme/Color.kt
+++ app/src/main/java/com/jujodevs/marvelcompose/ui/theme/Color.kt
@@ -2,10 +2,6 @@ package com.jujodevs.marvelcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Red = Color(0xFFE23636)
+val RedDark = Color(0xFF791111)
+val Blue = Color(0xFF518CCA)
--- app/src/main/java/com/jujodevs/marvelcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/jujodevs/marvelcompose/ui/theme/Theme.kt
@@ -10,21 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = Red,
+    secondary = Blue,
+    tertiary = RedDark,
+    onPrimary = Color.White,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = Red,
+    secondary = Blue,
+    tertiary = RedDark,
+    onPrimary = Color.White,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -57,7 +60,7 @@ fun MarvelComposeTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.tertiary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
